86464deb245de2d6889bf5919b3c88e0146ca5b7
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-10-05 19:08:13 +0300

    added beauty to the search form in the search.html

--- app.py
+++ app.py
@@ -14,18 +14,36 @@ def load_posts(filename):
         return json.load(fp)
 
 
+def get_post_by_id(uid: int, posts):
+    for post in posts:
+        if post['id'] == uid:
+            return post
+    raise IndexError
+
+
+# all posts
 @app.route('/')
 def main_feed():
     return render_template('main.html', posts=load_posts(POSTS_FILE_NAME))
 
 
+# search throughout the posts
 @app.route('/search/')
 def search():
     posts = load_posts(POSTS_FILE_NAME)
     results = []
-    if word := request.args.get('s').lower():
+    if word := request.args.get('s'):
+        word = word.lower()
         results = [post for post in posts if word in post['content'].lower()]
     return render_template('search.html', posts=results[:MAX_POSTS_IN_SEARCH], max_posts=len(results))
+
+
+# one post in detail
+@app.route('/posts/<int:uid>')
+def post(uid: int):
+    posts = load_posts(POSTS_FILE_NAME)
+    post = get_post_by_id(uid, posts)
+    return render_template('post.html', post=post)
 
 
 if __name__ == '__main__':
