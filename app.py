a64c5a012fbd017b66ebd5979666e171a6716d9e
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-10-05 19:41:12 +0300

    added the get_comments_by_post_id() func

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 POSTS_FILE_NAME = 'data/data.json'
+COMMENTS_FILE_NAME = 'data/comments.json'
 MAX_POSTS_IN_SEARCH = 10
 
 
-def load_posts(filename):
+def read_json(filename):
     with open(filename, encoding='utf-8') as fp:
         return json.load(fp)
 
@@ -21,6 +22,11 @@ def get_post_by_id(uid: int, posts):
     raise IndexError
 
 
+def get_comments_by_post_id(post_id: int):
+    comments = read_json(COMMENTS_FILE_NAME)
+    return [comment for comment in comments if comment['post_id'] == post_id]
+
+
 # all posts
 @app.route('/')
 def main_feed():
@@ -30,7 +36,7 @@ def main_feed():
 # search throughout the posts
 @app.route('/search/')
 def search():
-    posts = load_posts(POSTS_FILE_NAME)
+    posts = read_json(POSTS_FILE_NAME)
     results = []
     if word := request.args.get('s'):
         word = word.lower()
@@ -41,7 +47,7 @@ def search():
 # one post in detail
 @app.route('/posts/<int:uid>')
 def post(uid: int):
-    posts = load_posts(POSTS_FILE_NAME)
+    posts = read_json(POSTS_FILE_NAME)
     post = get_post_by_id(uid, posts)
     return render_template('post.html', post=post)
 
